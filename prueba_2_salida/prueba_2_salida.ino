fe20ab32a0c1a605a7f31d975a79230292ffe94d
jgualdronc <jgualdron@unal.edu.co>
2025-05-22 20:42:14 -0500

    se hiciieron un poco de cosas, todo funciona, pero hay que hacer un par de pruebas mas

--- prueba_2_salida/prueba_2_salida.ino
+++ prueba_2_salida/prueba_2_salida.ino
@@ -1,11 +1,15 @@
-#define ENCODER_A       18 // Amarillo
-#define ENCODER_B       19 // Verde
+#include <Wire.h>
+#include <Adafruit_ADS1X15.h>
+
+// Crear objeto ADS1115
+Adafruit_ADS1115 ads;
+volatile unsigned long lastEncoderTime = 0;
 // definiciones funcionamiento brazo
 //boton parar
 const int EMERGENCIA = 27;
 //
 const int MILLIS_ESPERA = -1;
-int PULSOS = 500;
+int PULSOS = 1500;
 int pulsos_a_dar = 0;
 int pulsos_acumulados = 0;
 long int delta_time = 0;
@@ -20,22 +24,26 @@ bool funcionando = 0;
 ////carro
 const int CARRO_MAS = 25; 
 const int CARRO_MENOS = 26;
+int destino_carrito = 0; // angulo
+float angulo_actual = -1;
+volatile bool dirreccion_carrito = true;
+volatile int direccion_carrito = 1;
 const int PWMCARRO = 5;
 const int canalPWM = 0;   // Canal de PWM (0-15 en ESP32)
 const int frecuencia = 5000; // Frecuencia en Hz (ajusta según tu motor)
 const int resolucion = 8; // Resolución de 8 bits (valores de 0 a 255)
 bool carro_andando = false;
 volatile int theta = 0;
-const int DURACION_CARRO_MILLIS = 1000;
+volatile int theta_1 = 0;
+volatile int theta_2 = 0;
+const int DURACION_CARRO_MILLIS = 3500;
 long int acumulado_carro = 0;
 ////arco
-//const int ARCO_MAS = 35;
-//const int ARCO_MENOS = 39;
 //// encoder
 const int POSICIONAL_CARRO = -1;
 volatile long posicion = 0;
-volatile int direccion = 0;
-float velocidad_carro = 128;
+volatile bool direccion = true;
+float velocidad_carro = 200;
 ////pot
 const int POTENCIOMETRO = 33;
 ////emergencia
@@ -72,12 +80,16 @@ int pulsos_dados = 0;
 ////posicionamiento
 //brazo
 float posbrazo = 0;
-float PULSEPERANG = 15;
+float PULSEPERANG = 44.44;
 //carro
 float poscarro = 0;
 float HITPERANG = 15;
 
+//////adc
 
+
+//Adafruit_ADS1115 ads;
+//////adc
 //pwm
 const int pwmChannel = 0;  // Canal PWM (0-15)
 const int freq = 5000;      // Frecuencia del PWM en Hz
@@ -87,41 +99,44 @@ struct DataPoint {
     float angle;
 };
 
-const int numPoints = 56; // Número de datos en la tabla
-DataPoint lookupTable[numPoints] = {
-    {399, 84.98688624}, {690, 80.19571472}, {1153, 71.47463182}, {1635, 62.39911574}, {2080, 58.08505994},
-    {2450, 53.48557076}, {3165, 46.7074986}, {3563, 41.03073654}, {3976, 38.17467354}, {4360, 30.56922412},
-    {4755, 27.60667785}, {4908, 26.46488383}, {4616, 27.82409638}, {4053, 33.84534026}, {3541, 40.965844},
-    {3110, 46.81419191}, {2764, 51.09586154}, {2414, 55.07549778}, {1983, 60.46121774}, {1569, 64.24472924},
-    {1225, 69.90801227}, {894, 76.3902681}, {664, 80.58658583}, {437, 84.88715022}, {299, 86.88698059},
-    {219, 88.74371497}, {-216, 91.7083151}, {-356, 94.06418578}, {-599, 98.28484138}, {-910, 103.7994854},
-    {-1244, 110.2248594}, {-1640, 114.3947088}, {-1916, 118.384609}, {-2299, 123.550662}, {-2629, 127.759981},
-    {-3059, 134.1586004}, {-3414, 136.7769871}, {-3775, 138.9692635}, {-4141, 141.0639508}, {-4467, 144.5470441},
-    {-4684, 147.9380563}, {-4327, 146.3099325}, {-3816, 140.9644871}, {-3340, 135.5721924}, {-2988, 132.2085802},
-    {-2649, 128.2901632}, {-2184, 122.0956482}, {-1929, 118.2677772}, {-1655, 115.1405829}, {-1336, 111.1485688},
-    {-1062, 107.2967718}, {-790, 101.7923712}, {-562, 97.79207808}, {-433, 95.51147742}, {-317, 93.51375026},
-    {-178, 91.00508601}
-};
-
-float interpolate(int position) {
-    for (int i = 0; i < numPoints - 1; i++) {
-        if (position >= lookupTable[i].position && position <= lookupTable[i + 1].position) {
-            float x0 = lookupTable[i].position;
-            float y0 = lookupTable[i].angle;
-            float x1 = lookupTable[i + 1].position;
-            float y1 = lookupTable[i + 1].angle;
-            return y0 + (y1 - y0) * (position - x0) / (x1 - x0);
-        }
-    }
-    // Si la posición está fuera de los límites, devuelve el valor más cercano
-    if (position < lookupTable[0].position) return lookupTable[0].angle;
-    if (position > lookupTable[numPoints - 1].position) return lookupTable[numPoints - 1].angle;
-    return 0; // En caso de error (no debería ocurrir)
+float angulo_calibrado(float voltaje){
+  float a = -15.5517; float b = 61.4428; float c = 105.6066;
+  float x = voltaje;
+  return a*x*x + b*x + c;
 }
 
+void debug_angulo(){
+  return;
+  int16_t adc0 = ads.readADC_SingleEnded(3);
+  float volts = ads.computeVolts(adc0);
+  angulo_actual = angulo_calibrado(volts);
+  return;
+}
+void calcular_angulo(){ //debug
+  float maximo = 4.19; float minimo = 0.98;
+  float ang_max = 15; float ang_min = 170;
+  int16_t adc0 = ads.readADC_SingleEnded(3);
+  float volts = ads.computeVolts(adc0);
+  float pendiente = (170.0-15.0)/(0.98-4.19);
+  angulo_actual = pendiente * (volts - 4.19) + 15;
+
+  // Limitar el ángulo a los valores dados
+  if (angulo_actual < ang_max) angulo_actual = ang_max;
+  if (angulo_actual > ang_min) angulo_actual = ang_min;
+
+  Serial.print("Voltios: ");
+  Serial.print(volts);
+  Serial.print(" -> Ángulo estimado: ");
+  Serial.println(angulo_actual);
+}
 
 void setup() {
   Serial.begin(115200);
+    if (!ads.begin()) {
+    Serial.println("Error al encontrar ADS1115");
+    while (1);
+  }
+  ads.setGain(GAIN_TWOTHIRDS);
   pinMode(BOT_ARCO_IZQ, INPUT);  // Configuramos el pin del botón como entrada
   attachInterrupt(digitalPinToInterrupt(BOT_ARCO_IZQ), button_4_red_arco, FALLING); 
   pinMode(BOT_ARCO_DER, INPUT);  // Configuramos el pin del botón como entrada
@@ -131,12 +146,7 @@ void setup() {
   pinMode(BOT_CARRO_DER, INPUT);  // Configuramos el pin del botón como entrada
   attachInterrupt(digitalPinToInterrupt(BOT_CARRO_DER), button_8_red_carro, FALLING); 
   pinMode(EMERGENCIA, INPUT);  // Configuramos el pin del botón como entrada
-  attachInterrupt(digitalPinToInterrupt(EMERGENCIA), button_black_emergencia, FALLING);
-  pinMode(ENCODERA, INPUT);  // Configuramos el pin del botón como entrada
-  attachInterrupt(digitalPinToInterrupt(ENCODERA), button_black_emergencia, FALLING);   
-  pinMode(ENCODER_A, INPUT);
-  pinMode(ENCODER_B, INPUT);
-  attachInterrupt(digitalPinToInterrupt(ENCODER_A),leerEncoder,RISING);  
+  attachInterrupt(digitalPinToInterrupt(EMERGENCIA), button_black_emergencia, FALLING);   
 
   //ledcSetup(pwmChannel, freq, resolution);  // Configura el canal PWM
   //ledcAttachPin(PWMCARRO, pwmChannel);        // Asigna el pin al canal PWM
@@ -144,6 +154,7 @@ void setup() {
 
   pinMode(PWMCARRO, OUTPUT);
   analogWrite(PWMCARRO, 255*0.2);  // 50% de 255 (8 bits) = 127
+  //ads.begin();
   //digitalWrite(PWMCARRO, 127);
 //Serial.println("hola_2");
   
@@ -167,6 +178,16 @@ void setup() {
 bool bandera_loop = false;
 int commaIndex = -1;
 void loop() {
+  int16_t adc0, adc1, adc2, adc3;
+  //adc0 = ads.readADC_SingleEnded(0);
+  //adc1 = ads.readADC_SingleEnded(1);
+  //adc2 = ads.readADC_SingleEnded(2);
+  //adc3 = ads.readADC_SingleEnded(3);
+  //Serial.print("AIN0: "); Serial.println(adc0);
+  //int16_t results = ads.readADC_Differential_0_1();
+  //Serial.print("Diferencial: "); 
+  //Serial.print(results);
+  //poscarro = float(theta_1)*cal_theta_1 - float(theta_2);
   if (automatico){
 
   }else{
@@ -184,132 +205,156 @@ void loop() {
       if(!digitalRead(PULSO)){
         pulsos_acumulados++;
         posicion_brazo += 1*direccion_brazo;
+        posbrazo += (1/PULSEPERANG) * direccion_brazo;
         if(pulsos_acumulados > pulsos_a_dar - 1){
+          Serial.println(pulsos_acumulados);
+          Serial.println(pulsos_a_dar);
+          Serial.println("apagando_arco");
           apagar_arco();
         }
       }
     }
   }
   if (carro_andando){
-    acumulado_carro += delta_time;
-    if (acumulado_carro > DURACION_CARRO_MILLIS){
-      acumulado_carro = 0;
-      apagar_carro();
+    calcular_angulo();
+    if(!automatico){
+      if (acumulado_carro > DURACION_CARRO_MILLIS){
+        acumulado_carro = 0;
+        apagar_carro();
+      }
+    }else{
+      if (direccion_carrito == 1){
+        if (angulo_actual > destino_carrito){
+          apagar_carro();
+        }
+      }else{
+        if (angulo_actual < destino_carrito){
+          apagar_carro();
+        }
+      }
     }
   }
 
   // Verifica si hay datos disponibles en el puerto serial
-if (Serial.available() > 0) {
-    bandera_loop = true;
-    // Lee la cadena hasta encontrar un salto de línea
-    String inputString = Serial.readStringUntil('\n');
-    
-    // Elimina espacios en blanco al inicio y final
-    inputString.trim();
-
-    // Verifica si la entrada contiene una coma (para azimut y zenit)
-    commaIndex = inputString.indexOf(',');
-    Serial.print(commaIndex);
-    if (commaIndex != -1) {
-      // 📌 Modo: Recepción de valores flotantes (azimut, zenit)
+  if (Serial.available() > 0) {
+      bandera_loop = true;
+      // Lee la cadena hasta encontrar un salto de línea
+      String inputString = Serial.readStringUntil('\n');
       
-      String azimutString = inputString.substring(0, commaIndex);
-      String zenitString = inputString.substring(commaIndex + 1);
+      // Elimina espacios en blanco al inicio y final
+      inputString.trim();
 
-      // Convierte a flotantes
-      float azimut = azimutString.toFloat();
-      float zenit = zenitString.toFloat();
-
-      // Verifica que no haya errores en la conversión
-      if (!(azimut == 0.0 && azimutString != "0") && 
-          !(zenit == 0.0 && zenitString != "0")) {}
+      // Verifica si la entrada contiene una coma (para azimut y zenit)
+      commaIndex = inputString.indexOf(',');
+      Serial.print(commaIndex);
+      if (commaIndex != -1) {
+        // 📌 Modo: Recepción de valores flotantes (azimut, zenit)
         
-        // 🟢 **Caso 1: Valores normales**
-        if (azimut >= 0 && zenit >= 0) {
-          automatico = true;
-          Serial.print("Azimut recibido: ");
-          Serial.println(azimut);
-          Serial.print("Zenit recibido: ");
-          Serial.println(zenit);
+        String azimutString = inputString.substring(0, commaIndex);
+        String zenitString = inputString.substring(commaIndex + 1);
 
-          float valores[2];
-          transformarCoordenadas(azimut, zenit, valores);
+        // Convierte a flotantes
+        float azimut = azimutString.toFloat();
+        float zenit = zenitString.toFloat();
 
-          Serial.print("Ángulo: ");
-          Serial.println(valores[0]);
-          Serial.print("Carrito: ");
-          Serial.println(valores[1]);
-          float diferencia = valores[0] - posbrazo;  // Diferencia con signo
-          int pulsos_a_dar = abs(diferencia) * PULSEPERANG;  // Conversión a pulsos
+        // Verifica que no haya errores en la conversión
+        if (!(azimut == 0.0 && azimutString != "0") && 
+            !(zenit == 0.0 && zenitString != "0")) {}
+          
+          // 🟢 **Caso 1: Valores normales**
+          if (azimut >= 0 && zenit >= 0) {
+            calcular_angulo();
+            automatico = true;
+            Serial.print("Azimut recibido: ");
+            Serial.println(azimut);
+            Serial.print("Zenit recibido: ");
+            Serial.println(zenit);
 
-          // Determinar dirección y mover
-          if (diferencia > 0) {
-              mover_arco(true);  // Dirección positiva
-          } else if (diferencia < 0) {
-              mover_arco(false);  // Dirección negativa
+            float valores[2];
+            transformarCoordenadas(azimut, zenit, valores);
+
+            Serial.print("Ángulo: ");
+            Serial.println(valores[0]);
+            Serial.print("Carrito: ");
+            Serial.println(valores[1]);
+            float angulo_a_ir_carrito = valores[1];
+            destino_carrito = angulo_a_ir_carrito + 90.0;
+            Serial.println("destino_carrito:");
+            Serial.println(destino_carrito);
+            Serial.println("angulo actual");
+            Serial.println(angulo_actual);
+            if (angulo_actual != destino_carrito){
+              if (angulo_actual > destino_carrito){
+                direccion_carrito = -1;
+                button_8_red_carro();
+              }else{
+                direccion_carrito = 1;
+                button_2_red_carro();
+              }
+            }
+            // apagar_carro();
+
+            float diferencia = -(valores[0] - posbrazo);  // Diferencia con signo
+            Serial.println(diferencia);
+            pulsos_a_dar = abs(diferencia) * PULSEPERANG;  // Conversión a pulsos
+            Serial.print(pulsos_a_dar);
+            Serial.println(" pulsos");
+            direccion_brazo = diferencia/abs(diferencia);
+
+            // Determinar dirección y mover
+            if (diferencia < 0) {
+              Serial.println("moviendo_arco");
+                mover_arco(false);  // Dirección positiva
+            } else {
+                mover_arco(true);  // Dirección negativa
+            }
+
+            // ✅ Actualizar la posición en base a los pulsos realmente ejecutados
+            Serial.print("posición: ");
+            Serial.println(posbrazo);
+
+          // 🔴 **Caso 2: Valores negativos -> Modo calibración**
+          } else if (azimut < 0.0 && zenit < 0.0) {
+        } else {}
+      }else{
+
+          // 📌 Modo: Recepción de enteros (comandos)
+        float valor_float = inputString.toFloat();
+
+        int inputNumber = inputString.toInt();  // Convierte la entrada a entero
+        if (float(inputNumber)/valor_float != 1){
+          //cal_theta_1 = valor_float;
+          inputNumber = -1;
+          Serial.println("calibracion_cambiada");
+        }
+
+        if ((inputNumber >= 2 && inputNumber <= 8)) {
+          Serial.print("Comando recibido: ");
+          Serial.println(inputNumber);
+          
+          switch (inputNumber) {
+            case 2:
+              button_2_red_carro();
+              break;
+            case 4:
+              button_4_red_arco();
+              break;
+            case 5:
+              button_black_emergencia();
+              break;
+            case 6:
+              button_6_red_arco();
+              break;
+            case 8:
+              button_8_red_carro();
+              break;
           }
-
-          // ✅ Actualizar la posición en base a los pulsos realmente ejecutados
-          posbrazo += (pulsos_a_dar / PULSEPERANG) * (diferencia > 0 ? 1 : -1);
-          Serial.print("Nueva posición actualizada del brazo: ");
-          Serial.println(posbrazo);
-
-        // 🔴 **Caso 2: Valores negativos -> Modo calibración**
-        } else if (azimut < 0.0 && zenit < 0.0) {
-          Serial.println("🔧 Modo Calibración: Procesando valores absolutos.");
-
-          float absAzimut = fabs(azimut);  // Convierte a positivo
-          float absZenit = fabs(zenit);
-
-          Serial.print("Azimut calibrado: ");
-          Serial.println(absAzimut);
-          Serial.print("Zenit calibrado: ");
-          Serial.println(absZenit);
-
-          float valoresCalibracion[2];
-          transformarCoordenadas(absAzimut, absZenit, valoresCalibracion);
-
-          Serial.print("Ángulo calibrado: ");
-          Serial.println(valoresCalibracion[0]);
-          posbrazo = valoresCalibracion[0];
-          Serial.print("Carrito calibrado: ");
-          Serial.println(valoresCalibracion[1]);
-          poscarro = valoresCalibracion[1];
+          digitalWrite(ledPin, HIGH);
+        } else {
+          Serial.println("❌ Error: Número fuera de rango.");
+          digitalWrite(ledPin, LOW);
         }
-
-      } else {
-
-      // 📌 Modo: Recepción de enteros (comandos)
-
-      int inputNumber = inputString.toInt();  // Convierte la entrada a entero
-
-      if (inputNumber >= 2 && inputNumber <= 8) {
-        Serial.print("Comando recibido: ");
-        Serial.println(inputNumber);
-        
-        switch (inputNumber) {
-          case 2:
-            button_2_red_carro();
-            break;
-          case 4:
-            button_4_red_arco();
-            break;
-          case 5:
-            button_black_emergencia();
-            break;
-          case 6:
-            button_6_red_arco();
-            break;
-          case 8:
-            button_8_red_carro();
-            break;
-        }
-        digitalWrite(ledPin, HIGH);
-      } else {
-        Serial.println("❌ Error: Número fuera de rango.");
-        digitalWrite(ledPin, LOW);
       }
-    }
   }
 }
 void button_black_emergencia(){
@@ -334,6 +379,8 @@ void button_8_red_carro(){
   mover_carro(false);
 }
 void mover_carro(bool direccion){
+  Serial.println("prendiendo_carro");
+  dirreccion_carrito = direccion;
   //Serial.println("hola_carro");
   carro_andando = true;
 
@@ -347,41 +394,25 @@ void mover_carro(bool direccion){
   }
 }
 void apagar_carro(){
+  Serial.println("apagando carro");
     carro_andando = false;
     analogWrite(CARRO_MAS, 0);
     analogWrite(CARRO_MENOS, 0);
+    calcular_angulo();
+    Serial.println("angulo_actual");
+    Serial.println(angulo_actual);
+    debug_angulo();
+    //Serial.println(cal_theta_1);
 }
 void cambiar_vel_carro(float valor){
   velocidad_carro = valor;
 }
-volatile long recorrido = 0;  // Variable para almacenar el recorrido
 
 // Variables internas para seguimiento del estado del encoder
-void leerEncoder(){
 
-  //Lectura de Posición  
 
-    int b = digitalRead(ENCODER_B);
-    if(b > 0){
-      //Incremento variable global
-      theta++;
-    }
-    else{
-      //Decremento variable global
-      theta--;
-    }
-  }
 
 // Rutina de interrupción para el canal B
-void IRAM_ATTR encoderB() {
-    if (digitalRead(ENCODERA) == HIGH) {
-        posicion--;  // Dirección antihoraria
-        direccion = -1;
-    } else {
-        posicion++;  // Dirección horaria
-        direccion = 1;
-    }
-}
 void mover_arco(bool direccion){
   //Serial.println("hola_arco");
       if (!digitalRead(ENABLE)){
@@ -393,9 +424,12 @@ void mover_arco(bool direccion){
       // Enciende el LED si el número es correcto
       digitalWrite(ledPin, direccion);
 }
+
 void apagar_arco(){
     pulsos_acumulados = 0;
     funcionando = 0;
+    automatico = false;
+    Serial.println(posbrazo);
 }
 
 // Función para convertir grados a radianes
